9c56beedc2afafbb3f91e75e12b523371898462e
Jaydegnan <jaydegnan@comcast.net>
2017-11-08 18:18:24 -0800

    questions 6 and 7

--- app.js
+++ app.js
@@ -1,49 +1,74 @@
 'use strict';
 
-var questionOne = prompt('Have I been to disneyworld?');
-if (questionOne.toLowerCase() === 'yes' || questionOne.toLowerCase() === 'y') {
+var userName = prompt('what is your name?')
+
+var questionOne = prompt('Have I been to disneyworld?').toLowerCase();
+if (questionOne === 'yes' || questionOne === 'y') {
   alert ('Its the happiest place on earth!');
   console.log('question one correct');
-} else if (questionOne.toLowerCase() === 'no' || questionOne.toLowerCase() === 'n') {
+} else if (questionOne === 'no' || questionOne === 'n') {
   alert ('Actually I have and it was magical');
   console.log('question one incorrect' );
 }
-var questionTwo = prompt('Do I have 6 siblings?');
-if (questionTwo.toLowerCase() === 'yes' || questionTwo.toLowerCase() === 'y') {
+var questionTwo = prompt('Do I have 6 siblings?').toLowerCase();
+if (questionTwo === 'yes' || questionTwo === 'y') {
   alert ('Unfortunately I only have 2');
   console.log('question two incorrect');
-} else if (questionTwo.toLowerCase() === 'no' || questionTwo.toLowerCase() === 'n') {
+} else if (questionTwo === 'no' || questionTwo === 'n') {
   alert ('You know me so well!');
   console.log('question two correct');
 }
-var questionThree = prompt('Am I african american?');
-if (questionThree.toLowerCase() === 'yes' || questionThree.toLowerCase() === 'y') {
+var questionThree = prompt('Am I african american?').toLowerCase();
+if (questionThree === 'yes' || questionThree === 'y') {
   alert ('Might wanna get your eyes checked!');
   console.log('question three incorrect');
-} else if (questionThree.toLowerCase() === 'no' || questionThree.toLowerCase() === 'n') {
+} else if (questionThree === 'no' || questionThree === 'n') {
   alert ('Nothing gets by you!');
   console.log('question three correct');
 
 }
 
-var questionFour = prompt('Do I like fishsticks?');
-if (questionFour.toLowerCase() === 'yes' || questionFour.toLowerCase() === 'y') {
+var questionFour = prompt('Do I like fishsticks?').toLowerCase();
+if (questionFour === 'yes' || questionFour === 'y') {
   alert ('love em!');
   console.log('question four correct');
-} else if (questionFour.toLowerCase() === 'no' || questionFour.toLowerCase() === 'n') {
+} else if (questionFour === 'no' || questionFour === 'n') {
   alert ('Who doesnt like fishsticks?!');
   console.log('question four incorrect');
 }
 
-var questionFive = prompt('Do I deserve a lump of coal for christmas?');
-if (questionFive.toLowerCase() === 'yes' || questionFive.toLowerCase() === 'y') {
+var questionFive = prompt('Do I deserve a lump of coal for christmas?').toLowerCase();
+if (questionFive === 'yes' || questionFive === 'y') {
   alert ('absolutely not you jerk!');
   console.log('question five incorrect');
-} else if (questionFive.toLowerCase() === 'no' || questionFive.toLowerCase() === 'n') {
+} else if (questionFive === 'no' || questionFive === 'n') {
   alert ('Hey I like you!');
   console.log('question question five  correct');
 }
 
+var count = 0;
+var myNum = 7;
+while (count < 4) {
+  var questionSix = prompt('What number am I thinking between 1 and 10');
+  if (questionSix > myNum) {
+    alert ('your guess is too high! try again');
+    count++;
+  } else if (questionSix < myNum) {
+    alert ('your guess is too low! try again');
+    count++;
+  } else {
+    alert ('that is correct! way to go');
+    count = 4;
+  }
+}
+
+var gameAnswers = ['league of legends', 'zelda', 'rocket league' , 'super smash brothers' , 'call of duty'];
+var questionSeven = prompt('What are my favorite video games?').toLowerCase();
+for (var i = 0; i <= 4; i++) {
+  if (questionSeven === gameAnswers[i]) {
+    alert('YIPPEE FOR YOU!, ' + userName + '.');
+  }
+}
 // if ((friend == 'y' || houseIsland === 'n') || (friend === 'yes' || houseIsland === 'no')) {
 // alert
 // }
